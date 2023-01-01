3b4e07e4ea35566b09b486d87694b1093fb954f9
merzaad <merzaad@gmail.com>
2023-01-01 19:15:42 +0330

    RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,15 @@ import '../styles/globals.css'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={styles.layout}>
-      <div className={styles.navbar}>HELLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOO</div>
-      {children}
-    </div>
+    <html>
+      <head />
+      <body>
+        <div className={styles.layout}>
+          <div className={styles.navbar}>HELLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOO</div>
+          {children}
+        </div>
+      </body>
+    </html>
   )
 }
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import styles from '../styles/page.module.scss'
+import { useRouter } from 'next/navigation'
 
 export default function Page() {
+  const router = useRouter()
+  const navigateToCprtyo = () => {
+    router.push('/cprtyo')
+  }
   return (
     <div className={styles.main}>
-      <div className={styles.section1}>Hello</div>
+      <div className={styles.section1} onClick={navigateToCprtyo}>
+        Cprtyo
+      </div>
     </div>
   )
 }
