ccbe13e1abdba9719228468134a0c438beb6ba88
merzaad <merzaad@gmail.com>
2023-01-01 19:37:58 +0330

    fettch server side

--- app/cprtyo/page.tsx
+++ app/cprtyo/page.tsx
@@ -1,5 +1,15 @@
 import styles from '../../styles/Cprtyo/page.module.scss'
 
-export default function Page() {
-  return <div className={styles.cprtyo}>testttttttttttttttttttt</div>
+const getData = async () => {
+  try {
+    const result = await fetch('https://api.blockchair.com/ethereum/stats', { cache: 'no-cache' })
+    return result.json()
+  } catch (error) {
+    return error
+  }
+}
+export default async function Page() {
+  const test = await getData()
+  const result = JSON.stringify(test.data) || test.message
+  return <div className={styles.cprtyo}>{result}</div>
 }
