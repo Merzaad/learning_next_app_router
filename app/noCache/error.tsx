0d1c1e6520fc618a1c8f57cbc90c727e3655a812
merzaad <merzaad@gmail.com>
2023-06-16 08:32:13 +0330

    remove experimental

--- app/noCache/error.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function Error({ error }: { error: Error }) {
-  return (
-    <div className='centeredFlex'>
-      <div className='card'>{error.message}</div>
-    </div>
-  )
-}
