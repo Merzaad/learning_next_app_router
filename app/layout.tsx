6e9eba2501992f003c6cd45b08bb1dc50ebf5d85
merzaad <merzaad@gmail.com>
2023-06-23 14:48:55 +0330

    fix css issue on refresh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 import Navbar from '../components/Navbar'
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'learning_app_router',
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import './globals.css'
-
 export default function Page() {
   return (
     <div className='flex gap-4 justify-center items-center border-1 border-cyan-500 p-2 '>
