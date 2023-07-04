4b54c5d14ce9b25403bcf15306c7400d0e730e3c
merzaad <merzaad@gmail.com>
2023-07-04 08:59:02 +0330

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,14 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html className='w-full h-full p-0 m-0'>
+      <head>
+        <link
+          rel='icon'
+          href='/favicon.ico?<generated>'
+          type='image/<generated>'
+          sizes='<generated>'
+        />
+      </head>
       <body className='w-full h-full p-0 m-0 flex p-4 gap-4'>
         <Navbar />
         <div className='grow p-4 bg-neutral-950 rounded-lg overflow-y-auto'>{children}</div>
